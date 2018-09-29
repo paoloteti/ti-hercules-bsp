a9ee754308d4aea62113ae18c5041750fe815542
Paolo Teti <paolo.teti@gmail.com>
2018-09-29 14:56:57 +0200

    CI: `cargo check` with `cargo build`
    Replace `cargo check` with `cargo build` to catch asm error too
    
    Signed-off-by: Paolo Teti <paolo.teti@gmail.com>

--- ci/script.sh
+++ ci/script.sh
@@ -7,8 +7,8 @@ main() {
     case $TARGET in
         armebv7r-none-eabi*)
             if [ $TRAVIS_RUST_VERSION = nightly ]; then
-                cargo check --target $TARGET
-                cargo check --target $TARGET --release
+                cargo build --target $TARGET
+                cargo build --target $TARGET --release
             fi
             ;;
 
