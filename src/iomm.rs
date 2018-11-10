6282cbde5c208f3a5da4dd5163f412df3257ca4d
Paolo Teti <paolo.teti@gmail.com>
2018-11-10 18:38:28 +0100

    pinmux: fix pinmux matrix setup
    We need to clear the byte of each group otherwise a double selection
    is silently rejected and multiplexer will used the default.
    
    Remove few unused function, add documentation and run rustfmt
    
    Signed-off-by: Paolo Teti <paolo.teti@gmail.com>

--- src/iomm.rs
+++ src/iomm.rs
@@ -1,28 +1,45 @@
+use pinmux;
 ///
 /// I/O Multiplexing and Control Module (IOMM)
 ///
 use vcell::VolatileCell;
-use pinmux;
 
 #[repr(C)]
 pub struct Iomm {
-    revision: VolatileCell<u32>,      	    // Revision
+    /// Revision
+    revision: VolatileCell<u32>,
+    /// Reserved
     _reserved1: [VolatileCell<u32>; 7],
-    endian: VolatileCell<u32>,         	    // Device Endianness
+    /// Device Endianness
+    endian: VolatileCell<u32>,
+    /// Reserved
     _reserved2: [VolatileCell<u32>; 5],
-    kicker0: VolatileCell<u32>,		        // Secret Key 0
-    kicker1: VolatileCell<u32>,		        // Secret Key 1
+    /// Secret Key 0
+    kicker0: VolatileCell<u32>,
+    /// Secret Key 1
+    kicker1: VolatileCell<u32>,
+    /// Reserved
     _reserved3: [VolatileCell<u32>; 40],
-    err_raw_status: VolatileCell<u32>, 	    // Error Raw Status / Set
-    err_enabled_status: VolatileCell<u32>,  // Error Enabled Status / Clear
-    err_enable: VolatileCell<u32>,    	    // Error Signaling Enable
-    err_enable_clr: VolatileCell<u32>, 	    // Error Signaling Enable Clear
+    /// Error Raw Status / Set
+    err_raw_status: VolatileCell<u32>,
+    /// Error Enabled Status / Clear
+    err_enabled_status: VolatileCell<u32>,
+    /// Error Signaling Enable
+    err_enable: VolatileCell<u32>,
+    /// Error Signaling Enable Clear
+    err_enable_clr: VolatileCell<u32>,
+    /// Reserved
     _reserved4: VolatileCell<u32>,
-    fault_address: VolatileCell<u32>, 	    // Fault Address
-    fault_status: VolatileCell<u32>,  	    // Fault Status
-    fault_clear: VolatileCell<u32>,   	    // Fault Clear
+    /// Fault Address
+    fault_address: VolatileCell<u32>,
+    /// Fault Status
+    fault_status: VolatileCell<u32>,
+    // Fault Clear
+    fault_clear: VolatileCell<u32>,
+    /// Reserved
     _reserved5: [VolatileCell<u32>; 4],
-    pinnmr: [VolatileCell<u32>; 47],        // Pin Multiplexing Control
+    /// Pin Multiplexing Control
+    pinnmr: [VolatileCell<u32>; 47],
 }
 const IOMM_BASE_ADDR: *const Iomm = 0xFFFF_EA00 as *const Iomm;
 
@@ -38,46 +55,28 @@ impl Iomm {
     /// Set a 64bits key to enable/disable pin muxing
     /// Key value is not trivial to avoid accidental pin muxing at
     /// runtime. For the same reason function is not public.
-    fn kicker(&self, open:bool) {
-	    if open {
-	        self.kicker0.set(0x83E7_0B13);
-	        self.kicker1.set(0x95A4_F1E0);
-	    } else {
+    fn kicker(&self, open: bool) {
+        if open {
+            self.kicker0.set(0x83E7_0B13);
+            self.kicker1.set(0x95A4_F1E0);
+        } else {
             self.kicker0.set(0x0);
-	        self.kicker1.set(0x0);
-	    }
+            self.kicker1.set(0x0);
+        }
     }
 
-    fn configure(&self, pin: pinmux::PinMux, active: bool) {
-        if active {
-            self.pinnmr[pin.group()].set(0x1 << pin.bit());
-	    } else {
-            let muxreg = self.pinnmr[pin.group()].get();
-            self.pinnmr[pin.group()].set(muxreg & !(0x1 << pin.bit()));
-	    }
-    }
-
-    /// Setup connection between pin and specified peripherals/function
-    /// Leaves rest of multiplexer setup intact.
-    pub fn setup_pin_function(&self, pin:pinmux::PinMux, active:bool) {
-        self.kicker(true);
-        self.configure(pin, active);
-	    self.kicker(false)
+    fn configure(&self, pin: pinmux::PinMux) {
+        let mut muxreg = self.pinnmr[pin.group()].get();
+        let bit = 0x1 << (pin.shift() + pin.option());
+        muxreg = (muxreg & !(0xFF << pin.shift())) | bit;
+        self.pinnmr[pin.group()].set(muxreg);
     }
 
     pub fn setup_pins(&self, pins: &[pinmux::PinMux]) {
         self.kicker(true);
         for p in pins.iter() {
-            self.configure(*p, true);
+            self.configure(*p);
         }
         self.kicker(false);
-    }
-
-    pub fn pin_set_function(&self, pin: pinmux::PinMux) {
-	    self.setup_pin_function(pin, true);
-    }
-
-    pub fn pin_clear_function(&self, pin: pinmux::PinMux)  {
-	    self.setup_pin_function(pin, false);
     }
 }
--- src/pinmux.rs
+++ src/pinmux.rs
@@ -1,48 +1,28 @@
-
 /// Map PINNMRx register to bit field
 ///
 /// Reference: TMS570LS31x/21x 16/32-Bit RISC Flash Microcontroller
 ///            Chapter 4.6 Signal Multiplexing and Control
 ///
 
-macro_rules! grp_bit {
-    ($pin: expr, $group: expr, $bit: expr) => {
-        (($pin as isize) << 24) | (($group as isize) << 16) | ($bit as isize)
-    }
+macro_rules! grp_option {
+    ($pin: expr, $group: expr, $shift: expr, $option: expr) => {
+        (($pin as isize) << 24)
+            | (($group as isize) << 16)
+            | (($shift as isize) << 8)
+            | ($option as isize)
+    };
 }
 
-
-#[derive(Copy,Clone)]
+#[derive(Copy, Clone)]
 #[allow(non_camel_case_types)]
 pub enum PinMux {
-    // PIN 1
-    Pin1_GIOB3 	        = grp_bit!(1, 0, 0),
-    // PIN 2
-    Pin2_GIOA0 	        = grp_bit!(2, 0, 8),
-    // PIN 3
-    Pin3_MIBSPI3NCS3 	= grp_bit!(3, 0, 16),
-    Pin3_I2C_SCL 	  	= grp_bit!(3, 0, 17),
-    Pin3_N2HET1_29   	= grp_bit!(3, 0, 18),
-    Pin3_NTZ1 	  	    = grp_bit!(3, 0, 19),
-    // PIN 4
-    Pin4_MIBSPI3NCS_2 	= grp_bit!(4, 0, 24),
-    Pin4_I2C_SDA 		= grp_bit!(4, 0, 25),
-    Pin4_HET1_27 		= grp_bit!(4, 0, 26),
-    Pin4_NTZ2 	        = grp_bit!(4, 0, 27),
-    // PIN 5
-    Pin5_GIOA1         = grp_bit!(5, 1, 0),
-    // PIN 6
-    Pin6_N2HET1_11     = grp_bit!(6, 1, 8),
-    Pin6_MIBSPI3NCS4   = grp_bit!(6, 1, 9),
-    Pin6_N2HET2_18     = grp_bit!(6, 1, 10),
-    Pin6_EPWM1SYNCO    = grp_bit!(6, 1, 13),
-    // PIN 9
-    Pin9_GIOA2         = grp_bit!(9, 2, 8),
-    Pin9_N2HET2_0      = grp_bit!(9, 2, 3),
-    Pin9_EQEP21        = grp_bit!(9, 2, 4),
+    Pin38_SCIRX = grp_option!(38, 7, 16, 1),
+    Pin39_SCITX = grp_option!(39, 8, 0, 1),
 
-    Pin38_SCIRX        = grp_bit!(38, 7, 17),
-    Pin39_SCITX        = grp_bit!(39, 8, 1),
+    Pin40_MIBSPI1NCS_2 = grp_option!(40, 8, 8, 0),
+    Pin40_N2HET1_19 = grp_option!(40, 8, 8, 1),
+
+    Pin55_GIOB2 = grp_option!(55, 9, 16, 2),
 }
 
 impl PinMux {
@@ -50,8 +30,12 @@ impl PinMux {
         (self as usize) >> 24
     }
 
-    pub fn bit(self) -> usize {
+    pub fn option(self) -> usize {
         (self as usize) & 0xff
+    }
+
+    pub fn shift(self) -> usize {
+        ((self as usize) >> 8) & 0xff
     }
 
     pub fn group(self) -> usize {
