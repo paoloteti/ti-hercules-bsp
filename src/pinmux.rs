d4ae7f99f364882c6ad17076334f33d6271aee6b
Paolo Teti <paolo.teti@gmail.com>
2018-11-10 19:34:51 +0100

    pinmux: add few definitions
    Signed-off-by: Paolo Teti <paolo.teti@gmail.com>

--- src/pinmux.rs
+++ src/pinmux.rs
@@ -15,14 +15,36 @@ macro_rules! grp_option {
 
 #[derive(Copy, Clone)]
 #[allow(non_camel_case_types)]
+#[rustfmt::skip]
 pub enum PinMux {
-    Pin38_SCIRX = grp_option!(38, 7, 16, 1),
-    Pin39_SCITX = grp_option!(39, 8, 0, 1),
+    Pin1_GIOB3         = grp_option!(1, 0, 0, 0),
+    Pin2_GIOA0         = grp_option!(2, 0, 8, 0),
+
+    Pin3_MIBSPI3NCS_3  = grp_option!(3, 0, 16, 0),
+    Pin3_I2C_SCL       = grp_option!(3, 0, 16, 1),
+    Pin3_H2HET1_29     = grp_option!(3, 0, 16, 2),
+    Pin3_zTZ1          = grp_option!(3, 0, 16, 3),
+
+    Pin4_MIBSPI3NCS_2  = grp_option!(4, 0, 24, 0),
+    Pin4_I2C_SDA       = grp_option!(4, 0, 24, 1),
+    Pin4_H2HET1_27     = grp_option!(4, 0, 24, 2),
+    Pin4_zTZ2          = grp_option!(4, 0, 24, 3),
+
+    Pin5_GIOA1         = grp_option!(5, 1, 0, 0),
+
+    Pin6_H2NET1_11     = grp_option!(6, 1, 8, 0),
+    Pin6_MIBSPI3NCS_4  = grp_option!(6, 1, 8, 1),
+    Pin6_H2NET2_18     = grp_option!(6, 1, 8, 2),
+
+    Pin9_GIOA2         = grp_option!(9, 2, 0, 0),
+
+    Pin38_SCIRX        = grp_option!(38, 7, 16, 1),
+    Pin39_SCITX        = grp_option!(39, 8, 0, 1),
 
     Pin40_MIBSPI1NCS_2 = grp_option!(40, 8, 8, 0),
-    Pin40_N2HET1_19 = grp_option!(40, 8, 8, 1),
+    Pin40_N2HET1_19    = grp_option!(40, 8, 8, 1),
 
-    Pin55_GIOB2 = grp_option!(55, 9, 16, 2),
+    Pin55_GIOB2        = grp_option!(55, 9, 16, 2),
 }
 
 impl PinMux {
