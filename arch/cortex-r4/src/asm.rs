dbf009fae903bafdf290d79403e6bcda3342f16b
Paolo Teti <paolo.teti@gmail.com>
2019-02-10 18:51:06 +0100

    cortex-r4: start removing asm code
    Use 'llvm.arm.hint' where possible to remove inline asm code.
    
    
    Signed-off-by: Paolo Teti <paolo.teti@gmail.com>

--- arch/cortex-r4/src/asm.rs
+++ arch/cortex-r4/src/asm.rs
@@ -1,41 +1,22 @@
-use core::ops::FnOnce;
-
-#[inline(always)]
-/// NOP instruction
-pub fn nop() {
-    unsafe {
-        asm!("nop" :::: "volatile");
-    }
+#[link_name = "llvm.arm.hint"]
+extern "C" {
+    fn hint(a: i32);
 }
 
+/// NOP instruction
 #[inline(always)]
+pub fn nop() {
+    unsafe { hint(0) };
+}
+
 /// WFI (Wait For Interrupt) makes the processor suspend
 /// execution (Clock is stopped) until one of the following
 /// events take place:
 ///    An IRQ interrupt
 ///    An FIQ interrupt
 ///    A Debug Entry request made to the processor.
+#[inline(always)]
 pub unsafe fn wfi() {
-    asm!("wfi" :::: "volatile");
+    hint(2);
 }
 
-#[inline(always)]
-/// Global interrupts disable
-pub unsafe fn interrupts_disable() {
-    asm!("cpsid i" :::: "volatile");
-}
-
-#[inline(always)]
-/// Global interrupts disable
-pub unsafe fn interrupts_enable() {
-    asm!("cpsie i" :::: "volatile");
-}
-
-pub unsafe fn atomic<F, R>(f: F) -> R
-    where F: FnOnce() -> R
-{
-    interrupts_disable();
-    let res = f();
-    interrupts_enable();
-    res
-}
